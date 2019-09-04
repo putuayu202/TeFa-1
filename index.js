9c1cd1fa7e47a8654a97f54e5345df9756ef6677
“putuayu202” <dwikydanov39@gmai.com>
2019-09-04 19:29:26 +0700

    04/08/2019

--- index.js
+++ index.js
@@ -1,13 +1,11 @@
 var http = require('http');
-var port = 8000;
+const port = process.env.PORT||9000
   
   http.createServer(function(req, res, next) {
-    res.writeHead(200, {'Content-type' : 'text/plain'})
+    res.writeHead(200, {'Content-type' : 'text/plain'});
     res.end("{'succes' : true, 'data' : 'index' 'massage' : 'hello world', 'status' : 200}");
-    console.log('yeey jadi...')
+    console.log('yeey jadi...');
   })
   
-  .listen(port, function() {
-    console.log('Sabar lagi listening, portnya 8000');
-  });
+  .listen(port);
 
